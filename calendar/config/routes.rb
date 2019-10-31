66fb6a02f4a37248b18674419ecc8cd0d1a2b0e6
Gyuwon Bae <gbae1@uncc.edu>
2019-10-31 17:40:42 +0000

    Add Sign up and Log in fuction

--- calendar/config/routes.rb
+++ calendar/config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-  
   root 'welcome#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+  
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
 end
