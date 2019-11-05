905ad3a1abec88ff1eafb20c0b31741b8e36e8df
brenna dwyer <bdwyer2@uncc.edu>
2019-11-05 02:10:31 +0000

    redirected login link to calendar index

--- calendar/app/controllers/calendar_controller.rb
+++ calendar/app/controllers/calendar_controller.rb
@@ -3,19 +3,19 @@ class CalendarController < ApplicationController
         @event = EventsCalendar.all
     end
    
-   # def show
-    #    @event = EventsCalendar.find(params[:id])
-    #end
-    #def new
-     #   @event = EventsCalendar.new
-    #end
-    #def create
-     #   @event = EventsCalendar.new(article_params)
-      #  
-       # if @event.save
-        #    redirect_to @EventsCalendar
-        #else
-         #   render 'new'
-        #end
-    #end
+    def show
+        @event = EventsCalendar.find(params[:id])
+    end
+    def new
+        @event = EventsCalendar.new
+    end
+    def create
+        @event = EventsCalendar.new(article_params)
+        
+        if @event.save
+            redirect_to @EventsCalendar
+        else
+            render 'new'
+        end
+    end
 end
