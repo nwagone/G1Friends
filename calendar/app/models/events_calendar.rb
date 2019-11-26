f2629ad2cb8244219b68382e648727ad7b32a566
Gyuwon Bae <gbae1@uncc.edu>
2019-11-26 19:11:59 +0000

    display name of event in calendar table

--- calendar/app/models/events_calendar.rb
+++ calendar/app/models/events_calendar.rb
@@ -1,4 +1,8 @@
 class EventsCalendar < ApplicationRecord
     belongs_to :user
     scope :upcoming, -> { where("date = ?", Date.today)}
+    
+    def start_time
+        self.events_calendars.date
+    end
 end
