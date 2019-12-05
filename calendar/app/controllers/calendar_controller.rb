e65828aa80ee12f6271bf8b38503bcda87012937
Gyuwon Bae <gbae1@uncc.edu>
2019-12-05 22:46:07 +0000

    Added custom color to event link in week and month calendar

--- calendar/app/controllers/calendar_controller.rb
+++ calendar/app/controllers/calendar_controller.rb
@@ -56,5 +56,5 @@ end
 
 private
     def calendar_params
-        params.require(:events_calendar).permit(:title, :date, :time_start, :time_end, :description, :color)
+        params.require(:events_calendar).permit(:title, :date, :time_start, :time_end, :description, :color, :show_time)
     end
